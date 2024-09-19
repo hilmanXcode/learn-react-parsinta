d41993c6f1049f72ecb61a9fb76f24f7e73be37f
hilmanXcode <hilmanxcode@gmail.com>
2024-09-19 18:26:23 +0700

    learn: fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
-import { useEffect, useState } from "react";
 import PlaceContentCenter from "./components/PlaceContentCenter";
 import Input from "./components/Input";
 import Button from "./components/Button";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Card from "./components/Card";
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [online, setOnline] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect ini akan terus di render ketika ada nya perubahan 
   useEffect(() => {
-    console.log('Im always rendered.');
-  });
-  
-  // useEffect ini hanya akan di render pada page pertama kali di buka
-  useEffect(() => {
-    console.log('Im rendered at first time');
-  }, []);
-
-  // useEffect ini akan terpanggil jika terjadi perubahan di props / variabel yang kita definisikan di deps[]
-  useEffect(() => {
-    console.log(`Im now ${online ? 'online' : 'offline'}`)
-  }, [online]);
-
-  const onKeyDown = (e) => {
-    console.log(e.code);
-  }
-
-  // useEffect ini digunakan untuk cleanup(try to see the difference)  
-  useEffect(() => {
-    window.addEventListener('keydown', onKeyDown);
-    return () => {
-      window.removeEventListener('keydown', onKeyDown);
+    const getUsers = async() => {
+      setLoading(true);
+      try {
+        const {data} = await axios('https://jsonplaceholder.typicode.com/users');
+        setUsers(data);
+        setLoading(false);
+      } catch(error){
+        console.log('Something went wrong.');
+        setLoading(false);
+      }
+      
     }
-  })
+
+    getUsers();
+  }, []);
 
   return( 
     <PlaceContentCenter>
-      <Input name={'name'} type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-      <Button onClick={() => setOnline(online => !online)} className={'bg-black'}>Toggle Online</Button>
+     <Card>
+      <Card.Title>Users: {users.length}</Card.Title>
+      <Card.Body>
+        {loading ? 'Loading...' : users.length ?
+        <ol>
+          {users.map((user) => (
+            <li key={user.id}>{user.name} ({user.username})</li>
+          ))}
+        </ol> : 
+        <div>
+          There are no users.
+        </div>
+        }
+        
+      </Card.Body>
+     </Card>
     </PlaceContentCenter>
   )
 
